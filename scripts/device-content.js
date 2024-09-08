97a43f084c583d68f068cbae928d9702b4145ed0
thangndgit <81653169+thangndgit@users.noreply.github.com>
2024-09-08 22:39:58 +0700

    fix pwa

--- scripts/device-content.js
+++ scripts/device-content.js
@@ -8,6 +8,7 @@ var mobileElements = document.querySelectorAll(".mobile");
 if (isMobileDevice()) {
   desktopElements.forEach((e) => (e.style.display = "none"));
   mobileElements.forEach((e) => (e.style.display = "block"));
+  if (screen.orientation && screen.orientation.lock) screen.orientation.lock("landscape");
 } else {
   desktopElements.forEach((e) => (e.style.display = "block"));
   mobileElements.forEach((e) => (e.style.display = "none"));
