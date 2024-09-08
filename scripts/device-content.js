4860ada1467a7445aa7efc85d2ba94e26749b87f
thangndgit <81653169+thangndgit@users.noreply.github.com>
2024-09-08 22:59:22 +0700

    fix

--- scripts/device-content.js
+++ scripts/device-content.js
@@ -2,17 +2,27 @@ function isMobileDevice() {
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
+function requestFullScreenAndLockOrientation() {
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen().then(() => {
+      screen.orientation.lock("landscape").catch((err) => {
+        window.alert("fail");
+        console.error("Không thể khóa xoay màn hình: ", err);
+      });
+    });
+  } else {
+    window.alert("hmm");
+    console.error("Trình duyệt không hỗ trợ chế độ toàn màn hình.");
+  }
+}
+
 var desktopElements = document.querySelectorAll(".desktop");
 var mobileElements = document.querySelectorAll(".mobile");
 
 if (isMobileDevice()) {
   desktopElements.forEach((e) => (e.style.display = "none"));
   mobileElements.forEach((e) => (e.style.display = "block"));
-  if (screen.orientation && screen.orientation.lock)
-    screen.orientation
-      .lock("landscape")
-      .then(() => window.alert("supported"))
-      .catch((err) => window.alert("Error"));
+  requestFullScreenAndLockOrientation();
 } else {
   desktopElements.forEach((e) => (e.style.display = "block"));
   mobileElements.forEach((e) => (e.style.display = "none"));
